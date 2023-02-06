1a3000602aaec1685cbef30c1bf6d139a7a330fe
Nicolas Liu <nicolasliu18@gmail.com>
2023-02-05 23:14:04 -0500

    first commit

---
+++ skillsTest.js
@@ -0,0 +1,21 @@
+const button = document.querySelector("#button");
+const calculator = document.querySelector("#calculator");
+const sleepTimes = document.querySelector("#sleepTimes");
+
+let wakeUpTimes = [];
+
+button.addEventListener("click", calculate);
+
+function calculate() {
+    let currentTime = new Date();
+    currentTime.setMinutes(currentTime.getMinutes() + 14);
+    for (let i = 1; i < 7; i++) {
+        currentTime.setHours(currentTime.getHours() + 1);
+        currentTime.setMinutes(currentTime.getMinutes() + 30);
+        wakeUpTimes.push(currentTime.toLocaleTimeString("en-US",
+        {hour: "2-digit", minute: "2-digit"}
+        ))
+    }
+    sleepTimes.innerText = wakeUpTimes.join(", ");
+    calculator.style.display = "block";
+}
